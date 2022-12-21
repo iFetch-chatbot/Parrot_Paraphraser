93dbc754ca268ec8d34446f349e162d2899cbe93
Pedro Azevedo <pedrojose.azevedo@farfetch.com>
2022-12-21 15:48:55 +0000

    add: diversity now has local variable

--- parrot/parrot.py
+++ parrot/parrot.py
@@ -12,7 +12,7 @@ class Parrot():
     self.model     = AutoModelForSeq2SeqLM.from_pretrained(model_tag, use_auth_token=False, local_files_only=self.local_files_only)
     self.adequacy_score = Adequacy(local_files_only=local_files_only)
     self.fluency_score  = Fluency(local_files_only=local_files_only)
-    self.diversity_score= Diversity()
+    self.diversity_score= Diversity(local_files_only=local_files_only)
 
   def rephrase(self, input_phrase, use_gpu=False, diversity_ranker="levenshtein", do_diverse=False, style=1, max_length=32, adequacy_threshold = 0.90, fluency_threshold = 0.90):
       if use_gpu:
